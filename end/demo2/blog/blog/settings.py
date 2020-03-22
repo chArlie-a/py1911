e0d671503aa97394196e10b3c2d46b8e113ff051
charlie <335176033@qq.com>
2020-03-22 21:36:22 +0800

    all

--- end/demo2/blog/blog/settings.py
+++ end/demo2/blog/blog/settings.py
@@ -80,9 +80,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER':'root',
+        'PASSWORD':'123456'
     }
 }
 
